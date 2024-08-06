18494fe1bff2c12ab87e1276354f9758bc4adb28
Josh Horton <Josh.Horton@newcastle.ac.uk>
2024-08-06 15:06:51 +0100

    fix test script

--- recipe/test_load.py
+++ recipe/test_load.py
@@ -1,5 +1,5 @@
 from openff.toolkit import ForceField
 
 
-ForceField("de-force-1.0.3.offxml")
-ForceField("de-force_unconstrained-1.0.3.offxml")
+ForceField("de-force-1.0.3.offxml", load_plugins=True)
+ForceField("de-force_unconstrained-1.0.3.offxml", load_plugins=True)
